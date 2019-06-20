7d245acac17055c1eba0aa5ea9ffa4224e41f5d3
Kyle Hurst <kyle.c.hurst@gmail.com>
2019-06-20 16:42:43 -0400

    Add exporter page and jspdf package

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'exporter', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'exporter', loadChildren: './exporter/exporter.module#ExporterPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/exporter/exporter.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exporter',
+  templateUrl: './exporter.page.html',
+  styleUrls: ['./exporter.page.scss'],
+})
+export class ExporterPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
