f7de29b543ffc7ac828c96b8409670821fb927f6
Kyle Hurst <kyle.c.hurst@gmail.com>
2019-06-20 17:16:56 -0400

    Save html as pdf

--- src/app/exporter/exporter.page.ts
+++ src/app/exporter/exporter.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-exporter',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExporterPage implements OnInit {
 
+  @ViewChild('content') content: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public downloadPDF(){
+    let doc = new jsPDF;
+
+    let specialElementHandlers = {
+      '#editor': function(element, renderer){
+        return true;
+      }
+    };
+
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML, 15, 15, {
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test2.pdf');
   }
 
 }
